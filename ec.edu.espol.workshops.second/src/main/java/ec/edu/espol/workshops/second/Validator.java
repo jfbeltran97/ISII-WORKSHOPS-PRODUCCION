74194855ed2e9b99c59feb93aaa5810a66882139
Ana Carrión Barbotó <anacbarboto@gmail.com>
2020-10-22 22:28:56 -0500

    Validator class modified

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Validator.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Validator.java
@@ -2,6 +2,6 @@ package ec.edu.espol.workshops.second;
 
 public class Validator {
 	public static boolean isLicenseValid(String license) {
-		return false;
+		return (license.substring(0,2).equals("080"))?true:false;
 	}
 }
