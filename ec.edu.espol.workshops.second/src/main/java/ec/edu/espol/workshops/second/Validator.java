076c79266c0d750695812a95ec969bb0b77097fd
Johnny Beltran <jfbm97@hotmail.com>
2020-10-23 00:15:02 -0500

    Fixed .classpath and refactored Validator.java

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Validator.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Validator.java
@@ -2,6 +2,7 @@ package ec.edu.espol.workshops.second;
 
 public class Validator {
 	public static boolean isLicenseValid(String license) {
-		return (license.substring(0,2).equals("080"))?true:false;
+		// dummy validation logic
+		return license.startsWith("080");
 	}
 }
