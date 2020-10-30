b9bc16852d7b00fd6b4106d6672985b9cd4d7a94
Johnny Beltran <jfbm97@hotmail.com>
2020-10-29 21:27:51 -0500

    Fixed PMD and Checkstyle errors and warnings

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -1,20 +1,34 @@
 package ec.edu.espol.workshops.second;
 
-public class CarInsurance {
-	public static final int BASE = 500;
-	
-	private CarInsurance() {
-		
-	}
-	
-	public static int calculatePremium(Customer c) {
-		if (!c.isValid()) return -1;
-		int premium = BASE;
-		if( c.ageBetween(45, 65) ) premium -= 100;
-		if( c.isFemale() || c.isMarried() )
-			premium -= 200;
-		else if ( c.isMale() && !c.isMarried() && c.getAge() < 25 )
-			premium += 1500;
-		return premium;
-	}
+/**
+ * This class is used to calculate the insurance for a customer.
+ * @author Johnny Beltrán
+ *
+ */
+public final class CarInsurance {
+
+  /**
+   * Base value from which the premium is calculated.
+   */
+  public static final int BASE = 500;
+
+  private CarInsurance() {
+    
+  }
+
+  /**
+   * Calculates premium based on customer properties.
+   * 
+   * @param c The customer for whom the value is calculated.
+   * @return The calculated premium if customer is valid, -1 otherwise.
+   */
+  public static int calculatePremium(Customer c) {
+    if (!c.isValid())
+        return -1;
+    int premium = BASE;
+    premium -= 100 * (c.ageBetween(45, 65) ? 1 : 0);
+    premium -= 200 * (c.isFemale() || c.isMarried() ? 1 : 0);
+    premium += 1500 * (c.isMale() && !c.isMarried() && c.getAge() < 25 ? 1 : 0);
+    return premium;
+  }
 }
