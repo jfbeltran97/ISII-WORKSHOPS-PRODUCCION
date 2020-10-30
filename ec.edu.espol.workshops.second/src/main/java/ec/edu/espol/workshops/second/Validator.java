b76dd5f2dc086513f63502d2ba977cd4424a584a
Ana Carrión Barbotó <anacbarboto@gmail.com>
2020-10-29 23:57:24 -0500

    pmd part 2

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Validator.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Validator.java
@@ -1,8 +1,4 @@
 package ec.edu.espol.workshops.second;
 
 public class Validator {
-	public static boolean isLicenseValid(String license) {
-		// dummy validation logic
-		return license.startsWith("080");
-	}
 }
